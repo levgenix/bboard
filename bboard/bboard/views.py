2969a73db1b405e8d41555b33203a753ea79b3cb
Evgeny V. Lobach <mr.lobach@gmail.com>
2020-07-08 11:52:42 +0300

    Filter by rubric added

--- bboard/bboard/views.py
+++ bboard/bboard/views.py
@@ -1,14 +1,17 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
 from django.shortcuts import render
 
-from .models import Bb
+from .models import Bb, Rubric
 
 
 def index(request):
-    #bbs = Bb.objects.order_by('-published')
-    #template = loader.get_template('bboard/index.html')
-    #context = {'bbs': bbs}
-    #return HttpResponse(template.render(context, request))
-    return render(request, 'bboard/index.html', {'bbs': Bb.objects.all()})
+    bbs = Bb.objects.all()
+    rubrics = Rubric.objects.all()
+    return render(request, 'bboard/index.html', {'bbs': bbs, 'rubrics': rubrics})
+
+
+def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
+    return render(request, 'bboard/by_rubric.html', context)
