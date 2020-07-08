c907ceca4e6fd82aa683c6cd1f2d42157d6ed5ef
Evgeny V. Lobach <mr.lobach@gmail.com>
2020-07-08 12:37:00 +0300

    Added form for add Bb

--- bboard/bboard/urls.py
+++ bboard/bboard/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import index, by_rubric
+from .views import index, by_rubric, BbCreateView
 
 
 urlpatterns = [
+    path('add/', BbCreateView.as_view(), name='add'),
     path('<int:rubric_id>/', by_rubric, name='by_rubric'),
     path('', index, name='index'),
 ]
